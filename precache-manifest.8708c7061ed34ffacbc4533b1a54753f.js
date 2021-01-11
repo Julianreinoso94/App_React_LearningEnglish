0c1ab24950589f0ee4478fa26613f5e1abda7f55
Julián <julianreinoso94@gmail.com>
2021-01-10 21:35:28 -0300

    Updates

---
+++ precache-manifest.8708c7061ed34ffacbc4533b1a54753f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77334cd3a4ea9e9e50841d8b8ad0487a",
+    "url": "/App_React_LearningEnglish/index.html"
+  },
+  {
+    "revision": "c14e902165c6880fc532",
+    "url": "/App_React_LearningEnglish/static/css/main.fe256f49.chunk.css"
+  },
+  {
+    "revision": "3e10aaf393ebc2c837b3",
+    "url": "/App_React_LearningEnglish/static/js/2.e5a7e45f.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/App_React_LearningEnglish/static/js/2.e5a7e45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14e902165c6880fc532",
+    "url": "/App_React_LearningEnglish/static/js/main.6d3b92aa.chunk.js"
+  },
+  {
+    "revision": "f038342004e17d7fd4b8",
+    "url": "/App_React_LearningEnglish/static/js/runtime-main.58379ab1.js"
+  }
+]);
